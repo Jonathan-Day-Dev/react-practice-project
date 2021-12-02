fc471b6d14eb0ae5e409cdefd9464abf84043617
Jonathan Day <jonday@jonday.dev>
2021-12-01 19:44:13 -0600

    added Card component and styles

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -1,22 +1,40 @@
-import styles from './AddUser.module.css';
+import styles from "./AddUser.module.css";
 
-const addUserHandler = e => {
-    e.preventDefault();
-}
+import Card from "../UI/Card";
+
+const addUserHandler = (e) => {
+  e.preventDefault();
+};
 
 const AddUser = (props) => {
   return (
-    <form onSubmit={addUserHandler} className={styles.form}>
-      <div className={styles['form-section']}>
-        <label htmlFor="username" className={styles['form-section__label']}>Username</label>
-        <input type="text" id="username" className={styles['form-section__input']} />
-      </div>
-      <div className={styles['form-section']}>
-        <label htmlFor="age" className={styles['form-section__label']}>Age</label>
-        <input type="number" id="age" className={styles['form-section__input']} />
-      </div>
-      <button type="submit" className={styles['form__btn']}>Add User</button>
-    </form>
+    <Card>
+      <form onSubmit={addUserHandler} className={styles.form}>
+        <div className={styles["form-section"]}>
+          <label htmlFor="username" className={styles["form-section__label"]}>
+            Username
+          </label>
+          <input
+            type="text"
+            id="username"
+            className={styles["form-section__input"]}
+          />
+        </div>
+        <div className={styles["form-section"]}>
+          <label htmlFor="age" className={styles["form-section__label"]}>
+            Age
+          </label>
+          <input
+            type="number"
+            id="age"
+            className={styles["form-section__input"]}
+          />
+        </div>
+        <button type="submit" className={styles["form__btn"]}>
+          Add User
+        </button>
+      </form>
+    </Card>
   );
 };
 
