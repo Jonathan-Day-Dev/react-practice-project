154f8705698514e4d7ee0630ab40be7de4235823
Jonathan Day <jonday@jonday.dev>
2021-12-02 07:32:44 -0600

    added portals for ErrorModal and Backdrop

--- src/components/Users/AddUser.js
+++ src/components/Users/AddUser.js
@@ -4,6 +4,7 @@ import styles from "./AddUser.module.css";
 import Card from "../UI/Card";
 import Button from "../UI/Button";
 import ErrorModal from "../UI/ErrorModal";
+import Wrapper from '../Helpers/Wrapper';
 
 const AddUser = (props) => {
   const [username, setUsername] = useState("");
@@ -44,7 +45,7 @@ const AddUser = (props) => {
   }
 
   return (
-    <div>
+    <Wrapper>
       {error && <ErrorModal title={error.title} message={error.message} onConfirm={errorHandler} />}
       <Card>
         <form onSubmit={addUserHandler} className={styles.form}>
@@ -75,7 +76,7 @@ const AddUser = (props) => {
           <Button type="submit">Add User</Button>
         </form>
       </Card>
-    </div>
+    </Wrapper>
   );
 };
 
